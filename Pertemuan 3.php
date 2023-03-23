7420a8c90d1d961691bdde0882c52e8a95a284cf
Andre2903 <128695158+Andre2903@users.noreply.github.com>
2023-03-23 17:43:08 +0700

    Update Pertemuan 3.php

--- Pertemuan 3.php
+++ Pertemuan 3.php
@@ -1,101 +1,128 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@Andregun29 
-ikhsanfzn
-/
-webprogramming
-Public
-Fork your own copy of ikhsanfzn/webprogramming
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-webprogramming/pertemuan4.php /
-@ikhsanfzn
-ikhsanfzn Add files via upload
-Latest commit f28071a 2 minutes ago
- History
- 1 contributor
-57 lines (47 sloc)  1.59 KB
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-$rows = 9; // jumlah baris pada pola segitiga
-for ($i = 1; $i <= $rows; $i++) { // loop untuk setiap baris
-    if ($i % 3 == 1) { // jika baris habis dibagi 3 dengan sisa 1
-        for ($j = 1; $j <= $i; $j++) { // loop untuk setiap kolom pada baris
-            echo "+ "; // mencetak tanda plus (+)
-        }
-    } elseif ($i % 3 == 2) { // jika baris habis dibagi 3 dengan sisa 2
-        for ($j = 1; $j <= $i; $j++) { // loop untuk setiap kolom pada baris
-            echo "- "; // mencetak tanda minus (-)
-        }
-    } else { // jika baris habis dibagi 3 dengan sisa 0
-        for ($j = 1; $j <= $i; $j++) { // loop untuk setiap kolom pada baris
-            echo "* "; // mencetak tanda bintang (*)
-        }
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+    <?php
+    $jenis_kelamin = 'Laki-laki';
+    $nama_lengkap = 'Andre Gunawan';
+    $umur = 24;
+    $berat = 55;
+    $tinggi = 170;
+    $menikah = false;
+    $nilai = 87;
+
+    if ($nilai >= 85 && $nilai <= 100) {
+        $huruf = "A";
+        $keterangan = "Istimewa";
+    } else if ($nilai >= 80 && $nilai <= 84) {
+        $huruf = "AB";
+        $keterangan = "Lebih dari Cukup";
+    } else if ($nilai >= 75 && $nilai <= 79) {
+        $huruf = "B";
+        $keterangan = "Baik";
+    } else if ($nilai >= 70 && $nilai <= 74) {
+        $huruf = "BC";
+        $keterangan = "Lebih dari Cukup";
+    } else if ($nilai >= 60 && $nilai <= 69) {
+        $huruf = "C";
+        $keterangan = "Cukup";
+    } else if ($nilai >= 50 && $nilai <= 59) {
+        $huruf = "CD";
+        $keterangan = "Kurang dari Cukup";
+    } else if ($nilai >= 40 && $nilai <= 49) {
+        $huruf = "D";
+        $keterangan = "Kurang";
+    } else if ($nilai <= 39) {
+        $huruf = "E";
+        $keterangan = "Gagal";
+    } else {
+        $huruf = "Nilai tidak terdefinisi. ";
+        $keterangan = '';
     }
-    echo "<br>"; // pindah ke baris berikutnya
-}
+    ?>
+    <h1>Data Diri Mahasiswa</h1>
+    <table border="2" style="border-collapse: collapse; margin-left: 2%;">
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>
+                <?php echo $nama_lengkap; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Jenis Kelamin
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?php echo $jenis_kelamin; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Umur
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?php echo $umur; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Status Pernikahan
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?= ($menikah) ? "Kawin" : "Belum Kawin"; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Nilai
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?php echo $nilai; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Huruf
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?php echo $huruf; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Keterangan
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?= $keterangan;?>
+            </td>
+        </tr>
+    </table>
+</body>
 
-
-    // for ($i = 0; $i < 3; $i++){
-    //     for ($j = 0; $j < 5; $j++){
-    //      echo "nilai ke-$i, $j <br>";
-    //     }
-    // }
-
-    // $poe = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'libur'];
-    // foreach ($poe as $key => $value){
-    //     echo "$value <br>";
-    // }
-
-    // $mams = ['permen', 'coklaaatttssssssss', 'batagor'];
-
-    // echo $mams[1];
-    // echo "<br>";
-    // echo $mams[0];
-    // echo "<br>";
-    // echo $mams[2];
-    // echo "<br>";
-
-    // for ($i = 0; $i < 10; $i++){
-    //     echo "Pengulangan ke-$i <br>";
-    // }
-
-    // $i = 11;  
-    // do  {
-    //     echo "Pengulangan ke-$i <br>";
-    //     $i++;
-    // } while ($i <= 10);
-
-    // while ($i < 10){
-    //     echo "Pengulangan ke-$i <br>";
-    //     $i++;
-    // }
-
-
-?>
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-webprogramming/pertemuan4.php at main · ikhsanfzn/webprogramming
+</html>
